ade6cc66360107e36785c6673b8d74279c263fb9
Chris Smith <chris.smith@pega.com>
2019-02-23 11:44:23 -0500

    Inital Commit

---
+++ bin/cli.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env node
+
+const ArgumentParser = require('argparse').ArgumentParser,
+  bolt = require('../lib/bolt'),
+  config = require('../lib/config'),
+  database = require('../lib/database'),
+  models = require('../lib/models');
+
+const parser = new ArgumentParser({
+  addHelp: true,
+  description: 'Builds the database of bolt metadata.',
+});
+
+parser.addArgument(
+  ['-v', '--verbose'],
+  {
+    defaultValue: false,
+    help: 'Print verbose debugging messages to stdout.',
+    nargs: 0,
+    action: 'storeTrue',
+  }
+);
+
+const args = parser.parseArgs(),
+  reader = new bolt.RepositoryReader(config),
+  writer = new database.MongooseWriter;
+
+database.connect().then(() => {
+  reader.getReleases().then((releases) => {
+    return writer.writeList(models.Release, releases, (artifact, release) => {
+      artifact.dependsOn(reader.getPackages(release.tag_name).then((packages) => {
+        return writer.writeList(models.PackageRelease, packages, (artifact, pkg) => {
+          artifact.dependsOn(writer.writeList(models.ComponentRelease, pkg.definitions, (artifact, definition, name) => {
+            const id = name + release.tag_name;
+            console.log("Writing component-release: " + id);
+            artifact.set({
+              id: id,
+              attributes: {
+                name: name,
+                version: pkg.version,
+                definition: deepClean(definition),
+                package: pkg.name + ':' + release.tag_name,
+                release: release.tag_name,
+              },
+            });
+          }).dependency((transaction) => {
+            const id = pkg.name + release.tag_name;
+            console.log("Writing package-release: " + id);
+
+            artifact.set({
+              id: id,
+              attributes: {
+                name: pkg.name,
+                version: pkg.version,
+                components: transaction.completedIds,
+                release: release.tag_name,
+              },
+            });
+          }));
+        }).dependency((transaction) => {
+          console.log("Writing release: " + release.tag_name);
+
+          artifact.set({
+            id: release.tag_name,
+            attributes: {
+              title: release.name,
+              changelog: release.body,
+              tag: release.tag_name,
+              branch: release.target_commitish,
+              documentation: 'https://' + release.tag_name.replace(/\./g, '-') + '.boltdesignsystem.com',
+              created: release.created_at,
+              published: release.published_at,
+              snapshot: release.tarball_url,
+              packages: transaction.completedIds,
+            },
+          });
+        });
+      }));
+    }).dependency((transaction) => {
+      console.log("Finished writing releases.");
+      process.exit();
+    });
+  });
+});
---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express"),
+  config = require("./lib/config"),
+  database = require("./lib/database"),
+  jsonapi = require("./lib/jsonapi");
+
+database.connect().then(() => {
+  const app = express(),
+    types = 'releases|package-releases|component-releases'
+
+  app.get("/", jsonapi.front.docsRequest);
+
+  app.get("/:type(" + types + ")", jsonapi.handler([
+    new jsonapi.query.Pagination(),
+  ]));
+
+  app.get("/:type(" + types + ")/:id", jsonapi.handler([
+    new jsonapi.query.Pagination(),
+  ]));
+
+  app.use("/:type(releases)/:id/relationships/components", (req, res, next) => {
+    res.json({
+      'links': {
+        "self": req.protocol + "://" + req.get('host') + req.originalUrl,
+        "related": config.server.host + "/component-releases?filter=(release,`" + req.params.id + "`)",
+      },
+    });
+  });
+
+  app.get("/:type(" + types + ")/:id/relationships/:relationship", jsonapi.handler([
+    new jsonapi.query.Pagination(),
+  ]));
+
+  app.get('/:type(releases)/:id/packages', jsonapi.handler([
+    new jsonapi.query.RelatedQuery("package-releases", "release"),
+    new jsonapi.query.Pagination(),
+  ]));
+
+  app.get('/:type(package-releases)/:id/components', jsonapi.handler([
+    new jsonapi.query.RelatedQuery("component-releases", "release"),
+    new jsonapi.query.Pagination(),
+  ]));
+
+  app.listen(config.server.port);
+
+  console.log("Bolt Discovery: Listening on port " + config.server.port);
+}).catch((err) => {
+  console.log("Couldn't start listening:");
+  console.log(err);
+});
